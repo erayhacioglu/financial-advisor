1feff792a9ab833ced598aa294bfabe3ad0f196b
erayhacioglu <hacoglueray@gmail.com>
2022-03-12 15:44:08 +0300

    nodemailer is update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
 
 	const transporter = nodemailer.createTransport({
 		host: 'smtp.gmail.com',
-		port: 587,
+		port: 465,
 		secure: true,
 		auth: {
 			user: process.env.user,
